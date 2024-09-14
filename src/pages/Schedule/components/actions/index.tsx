580c291a4e24cf7a28223df517268d0f50da27ee
Luiz Sá <luizinho.eduardo33@gmail.com>
2024-09-14 12:43:22 -0300

    feat: add modal

--- src/pages/Schedule/components/actions/index.tsx
+++ src/pages/Schedule/components/actions/index.tsx
@@ -1,5 +1,5 @@
 import { Appointment } from '@/core'
-import { Button } from '@/ui-components'
+import { Button, Form, Modal } from '@/ui-components'
 import React from 'react'
 import { FiEdit2, FiTrash2 } from 'react-icons/fi'
 
@@ -10,9 +10,21 @@ interface ActionsProps {
 const Actions: React.FC<ActionsProps> = ({ item }) => {
   return (
     <div className="flex gap-1">
-      <Button typeColor="alert">
-        <FiEdit2 />
-      </Button>
+      <Modal>
+        <Modal.Trigger>
+          <Button typeColor="alert">
+            <FiEdit2 />
+          </Button>
+        </Modal.Trigger>
+        <Modal.Content
+          title="Editar Agendamento"
+          footerContent={<Button>Fechar</Button>}
+        >
+          <div>
+            <Form.Input type="text" name="teste" label="teste" />
+          </div>
+        </Modal.Content>
+      </Modal>
       <Button typeColor="error">
         <FiTrash2 />
       </Button>
